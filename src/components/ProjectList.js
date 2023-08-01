0c19d77a63760b1b8b80781fe22c63d3101d817a
Qing Bin Liang <leunghb@hotmail.com>
2023-07-31 21:06:50 -0700

    complete lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          /* Comments should be outside of the JSX code */
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.description} // Changed 'description' to 'about'
+            technologies={project.technologies} // Added 'technologies' prop
+            url={project.url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
